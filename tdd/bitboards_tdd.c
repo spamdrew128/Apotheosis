0af261038dba0c2c88d203dcd4f0d8141a8c63b1
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2022-12-28 21:58:03 -0500

    fixing tdd

--- tdd/bitboards_tdd.c
+++ tdd/bitboards_tdd.c
@@ -5,8 +5,23 @@
 #include "bitboards_tdd.h"
 #include "bitboards.h"
 
-// static void  
+static void PrintBitboard() {
+
+}
+
+static void CanCountPopulation() {
+    Bitboard_t b = C64(0b110110010111010);
+    Population_t expected = 9;
+    PrintResults(PopulationCount(b) == expected);
+}
+
+static void CanIdentifyLSB() {
+    Bitboard_t b = C64(0b111000);
+    Square_t expected = d1;
+    PrintResults(PopulationCount(b) == expected);
+}
 
 void BitboardsTDDRunner() {
-
+    CanCountPopulation();
+    CanIdentifyLSB();
 }
