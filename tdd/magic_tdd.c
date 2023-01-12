ad54c3d9d338974ac932db1fe94ce4c4fc010e5b
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2023-01-11 19:38:59 -0500

    bishop mask test

--- tdd/magic_tdd.c
+++ tdd/magic_tdd.c
@@ -10,9 +10,10 @@ enum {
     edge_rook_index_bits = 11,
     middle_rook_index_bits = 10,
 
-    corner_bishop_index_bits = 6,
-    edge_bishop_index_bits = 5,
-    middle_bishop_index_bits = 10
+    bishop_9index_bits_area = SquareToBitset(d4) | SquareToBitset(e4) | SquareToBitset(d5) | SquareToBitset(e5),
+    bishop_7index_bits_area = 0x00003C24243C0000,
+    bishop_6index_bits_area = 0x8100000000000081,
+    bishop_5index_bits_area = 0x7EFFC3C3C3C3FF7E
 };
 
 // HELPERS
@@ -22,6 +23,10 @@ static bool SquareIsCorner(Square_t square) {
 
 static bool SquareIsEdge(Square_t square) {
     return CreateBitboard(1, square) & (a_file | h_file | rank_1 | rank_8);
+}
+
+static bool SquareIsInBishopArea(Square_t square, Bitboard_t area) {
+    return CreateBitboard(1, square) & area;
 }
 
 // TESTS
@@ -42,6 +47,27 @@ static void RookMasksMatch(MagicEntry_t rMagicEntries[NUM_SQUARES]) {
     PrintResults(success);
 }
 
+static void BishopsMasksMatch(MagicEntry_t rMagicEntries[NUM_SQUARES]) {
+    bool success = true;
+
+    for(int square = 0; square < NUM_SQUARES; square++) {
+        Bitboard_t mask = rMagicEntries[square].mask;
+        if(SquareIsInBishopArea(square, bishop_9index_bits_area)) {
+            success = success && (PopulationCount(mask) == 9);
+        } else if(SquareIsInBishopArea(square, bishop_7index_bits_area)) {
+            success = success && (PopulationCount(mask) == 7);
+        } else if(SquareIsInBishopArea(square, bishop_6index_bits_area)) {
+            success = success && (PopulationCount(mask) == 6);
+        } else if(SquareIsInBishopArea(square, bishop_5index_bits_area)) {
+            success = success && (PopulationCount(mask) == 5);
+        } else {
+            success = false;
+        }
+    }
+
+    PrintResults(success);
+}
+
 void MagicTDDRunner() {
     // These tests suck to write so I'm not gonna do it for magics.
     // Sue me.
@@ -52,6 +78,7 @@ void MagicTDDRunner() {
     InitBishopEntries(bMagicEntries);
 
     RookMasksMatch(rMagicEntries);
+    BishopsMasksMatch(bMagicEntries);
 
     FreeMagicEntries(rMagicEntries);
 }
