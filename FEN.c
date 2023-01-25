d2608dcc8c96e5028ebe8853ca8325563ffdd4b4
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2023-01-24 22:27:20 -0500

    FEN refactor

--- FEN.c
+++ FEN.c
@@ -88,15 +88,6 @@ void InterpretFEN(FEN_t fen, BoardInfo_t* info) {
         i++;
     }
 
-    for(int i = 0; i < 2; i++) {
-        info->allPieces[i] = 
-            info->pawns[i] |
-            info->knights[i] |
-            info->bishops[i] |
-            info->rooks[i] |
-            info->queens[i] |
-            info->kings[i];
-    }
-
-    info->empty = ~(info->allPieces[white] | info->allPieces[black]);
+    UpdateAllPieces(info);
+    UpdateEmpty(info);
 }
