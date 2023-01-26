01a47ec100b15deb143a5d7730e8c5f186c8c587
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2023-01-25 23:33:04 -0500

    I need sleep

--- movegen.c
+++ movegen.c
@@ -5,6 +5,8 @@
 typedef Bitboard_t (*UnsafeSquaresCallback_t)(BoardInfo_t* boardInfo);
 static UnsafeSquaresCallback_t UnsafeSquaresCallbacks[2] = { WhiteUnsafeSquares, BlackUnsafeSquares };
 
+typedef Bitboard_t (*SliderCaptureTargetsCallback_t)(Square_t square, Bitboard_t empty, Bitboard_t enemyPieces);
+
 #define CurrentMove(moveListPtr) \
     moveListPtr->moves[moveListPtr->maxIndex]
 
@@ -23,6 +25,27 @@ static void SerializeMovesIntoMovelist(MoveList_t* moveList, Bitboard_t newMoves
     }
 }
 
+static void SerializeSliderPostionsIntoMoves(
+    MoveList_t* moveList,
+    Bitboard_t sliders,
+    Bitboard_t restriction,
+    Bitboard_t empty,
+    Bitboard_t enemyPieces,
+    Piece_t pieceType,
+    SliderCaptureTargetsCallback_t SliderCaptureTargetsCallback
+) 
+{
+    while(sliders) {
+        Square_t sliderSquare = LSB(sliders);
+        Bitboard_t sliderCaptures = 
+            SliderCaptureTargetsCallback(sliderSquare, empty, enemyPieces) & restriction;
+
+        SerializeMovesIntoMovelist(moveList, sliderCaptures, sliderSquare, pieceType);
+
+        ResetLSB(sliders);
+    }
+}
+
 static void AddKingMoves(MoveList_t* moveList, BoardInfo_t* boardInfo, Bitboard_t unsafeSquares, Color_t color) {
     Bitboard_t kingMoves = KingMoveTargets(LSB(boardInfo->kings[color]), boardInfo->empty);
     Bitboard_t kingLegalMoves = KingLegalMoves(kingMoves, unsafeSquares);
@@ -30,9 +53,119 @@ static void AddKingMoves(MoveList_t* moveList, BoardInfo_t* boardInfo, Bitboard_
 }
 
 static void AddKingCaptures(MoveList_t* moveList, BoardInfo_t* boardInfo, Bitboard_t unsafeSquares, Color_t color) {
-    Bitboard_t kingCaptures = KingCaptureTargets(LSB(boardInfo->kings[color]), boardInfo->allPieces[!color]);
+    Bitboard_t kingSquare = LSB(boardInfo->kings[color]);
+    Bitboard_t kingCaptures = KingCaptureTargets(kingSquare, boardInfo->allPieces[!color]);
     Bitboard_t kingLegalCaptures = KingLegalMoves(kingCaptures, unsafeSquares);
-    SerializeMovesIntoMovelist(moveList, kingLegalCaptures, boardInfo->kings[color], king);
+    SerializeMovesIntoMovelist(moveList, kingLegalCaptures, kingSquare, king);
+}
+
+static void AddKnightCaptures(MoveList_t* moveList, BoardInfo_t* boardInfo, Bitboard_t allPins, Bitboard_t checkmask, Color_t color) {
+    Bitboard_t freeKnights = boardInfo->knights[color] & ~allPins;
+
+    while(freeKnights) {
+        Square_t knightSquare = LSB(freeKnights);
+        Bitboard_t knightCaptures = KnightCaptureTargets(knightSquare, boardInfo->allPieces[!color]) & checkmask;
+
+        SerializeMovesIntoMovelist(moveList, knightCaptures, knightSquare, knight);
+
+        ResetLSB(freeKnights);
+    }
+}
+
+static void AddBishopCaptures(MoveList_t* moveList, BoardInfo_t* boardInfo, Bitboard_t d12Pins, Bitboard_t checkmask, Color_t color) {
+    Bitboard_t freeBishops = boardInfo->bishops[color] & ~d12Pins;
+    Bitboard_t pinnedBishops = boardInfo->bishops[color] & d12Pins;
+    
+    Bitboard_t enemy = boardInfo->allPieces[!color];
+
+    SerializeSliderPostionsIntoMoves(
+        moveList,
+        freeBishops,
+        checkmask,
+        boardInfo->empty,
+        enemy,
+        bishop,
+        BishopCaptureTargets
+    );
+
+    SerializeSliderPostionsIntoMoves(
+        moveList,
+        pinnedBishops,
+        checkmask & d12Pins,
+        boardInfo->empty,
+        enemy,
+        bishop,
+        BishopCaptureTargets
+    );
+}
+
+static void AddRookCaptures(MoveList_t* moveList, BoardInfo_t* boardInfo, Bitboard_t hvPins, Bitboard_t checkmask, Color_t color) {
+    Bitboard_t freeRooks = boardInfo->rooks[color] & ~hvPins;
+    Bitboard_t pinnedRooks = boardInfo->rooks[color] & hvPins;
+    
+    Bitboard_t enemy = boardInfo->allPieces[!color];
+
+    SerializeSliderPostionsIntoMoves(
+        moveList,
+        freeRooks,
+        checkmask,
+        boardInfo->empty,
+        enemy,
+        rook,
+        RookCaptureTargets
+    );
+
+    SerializeSliderPostionsIntoMoves(
+        moveList,
+        pinnedRooks,
+        checkmask & hvPins,
+        boardInfo->empty,
+        enemy,
+        rook,
+        RookCaptureTargets
+    );
+}
+
+static void AddQueenCaptures(MoveList_t* moveList, BoardInfo_t* boardInfo, Bitboard_t hvPins, Bitboard_t d12Pins, Bitboard_t checkmask, Color_t color) {
+    Bitboard_t freeQueens = boardInfo->queens[color] & ~(hvPins | d12Pins);
+    Bitboard_t hvPinnedQueens = boardInfo->queens[color] & hvPins;
+    Bitboard_t d12PinnedQueens = boardInfo->queens[color] & d12Pins;
+    
+    Bitboard_t enemy = boardInfo->allPieces[!color];
+
+    SerializeSliderPostionsIntoMoves(
+        moveList,
+        freeQueens,
+        checkmask,
+        boardInfo->empty,
+        enemy,
+        queen,
+        QueenCaptureTargets
+    );
+
+    SerializeSliderPostionsIntoMoves(
+        moveList,
+        hvPinnedQueens,
+        checkmask & hvPins,
+        boardInfo->empty,
+        enemy,
+        queen,
+        RookCaptureTargets
+    );
+
+    SerializeSliderPostionsIntoMoves(
+        moveList,
+        d12PinnedQueens,
+        checkmask & hvPins,
+        boardInfo->empty,
+        enemy,
+        queen,
+        BishopCaptureTargets
+    );
+}
+
+static void AddPawnCaptures(MoveList_t* moveList, BoardInfo_t* boardInfo, Bitboard_t d12Pins, Bitboard_t allPins, Bitboard_t checkmask, Color_t color) {
+    Bitboard_t pawnCaptures = PawnCa
 }
 
 void CapturesMovegen(MoveList_t* moveList, BoardInfo_t* boardInfo, Color_t color) {
@@ -53,11 +186,14 @@ void CapturesMovegen(MoveList_t* moveList, BoardInfo_t* boardInfo, Color_t color
     
     Bitboard_t pinmaskList[NUM_DIRECTIONS];
     DefinePinmasks(boardInfo, color, pinmaskList);
-    Bitboard_t horizontalPins = pinmaskList[E] | pinmaskList[W];
-    Bitboard_t verticalPins = pinmaskList[N] | pinmaskList[S];
-    Bitboard_t d1Pins = pinmaskList[NE] | pinmaskList[SW];
-    Bitboard_t d2Pins = pinmaskList[SW] | pinmaskList[SE];
-    Bitboard_t allPins = horizontalPins | verticalPins | d1Pins | d2Pins;
+    Bitboard_t hvPins = pinmaskList[N] | pinmaskList[E] | pinmaskList[S] | pinmaskList[W];
+    Bitboard_t d12Pins = pinmaskList[NE] | pinmaskList[SE] | pinmaskList[NW] | pinmaskList[SW];
+    Bitboard_t allPins = hvPins | d12Pins;
+    
+    AddKnightCaptures(moveList, boardInfo, allPins, checkmask, color);
+    AddBishopCaptures(moveList, boardInfo, d12Pins, checkmask, color);
+    AddRookCaptures(moveList, boardInfo, hvPins, checkmask, color);
+    AddQueenCaptures(moveList, boardInfo, hvPins, d12Pins, checkmask, color);
 }
 
 void CompleteMovegen(MoveList_t* moveList, BoardInfo_t* boardInfo, Color_t color) {
