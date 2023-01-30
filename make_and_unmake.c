e9ab185660fb6e05172c4fd63b92d72ada54a8c6
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2023-01-29 19:57:39 -0500

    omw

--- make_and_unmake.c
+++ make_and_unmake.c
@@ -1,14 +1,70 @@
 #include "make_and_unmake.h"
+#include "game_state.h"
+#include "lookup.h"
 
-static void CastlingMakeHandler(BoardInfo_t* boardInfo, Move_t move, Color_t color) {
+static void RemoveCapturedPiece(
+    BoardInfo_t* boardInfo,
+    Bitboard_t* infoField,
+    Square_t capturedSquare,
+    Color_t color
+) 
+{
+    Bitboard_t caputuredBB = GetSingleBitset(capturedSquare);
+    ResetBits(infoField, caputuredBB);
+    ResetBits(&(boardInfo->allPieces[color]), caputuredBB);
+}
+
+static void UpdateBoardInfoField(
+    BoardInfo_t* boardInfo,
+    Bitboard_t* infoField,
+    Bitboard_t fromBB,
+    Bitboard_t toBB,
+    Color_t color
+) 
+{
+    ResetBits(infoField, fromBB);
+    SetBits(infoField, toBB);
+
+    ResetBits(&(boardInfo->allPieces[color]), fromBB);
+    SetBits(&(boardInfo->allPieces[color]), toBB);
+
+    UpdateEmpty(boardInfo);
+}
+
+static void MakeCastlingHandler(BoardInfo_t* boardInfo, Move_t move, Color_t color) {
     Square_t fromSquare = ReadFromSquare(move);
     Square_t toSquare = ReadToSquare(move);
 
+    Bitboard_t kingBB = boardInfo->kings[color];
+
     if(toSquare < fromSquare) { // queenside castle
-        GenShiftWest(boardInfo->kings[color], 2);
+        boardInfo->kings[color] = GenShiftWest(kingBB, 2);
+        ResetBits(&(boardInfo->allPieces[color]), kingBB);
+        
+        UpdateBoardInfoField(
+            boardInfo,
+            &(boardInfo->rooks[color]),
+            GenShiftWest(kingBB, 4),
+            GenShiftWest(kingBB, 1),
+            color
+        );
+
     } else {
-        GenShiftEast(boardInfo->kings[color], 2);
+        boardInfo->kings[color] = GenShiftEast(kingBB, 2);
+        ResetBits(&(boardInfo->allPieces[color]), kingBB);
+
+        UpdateBoardInfoField(
+            boardInfo,
+            &(boardInfo->rooks[color]),
+            GenShiftEast(kingBB, 3),
+            GenShiftEast(kingBB, 1),
+            color
+        );
     }
+
+    GameState_t nextState = GetDefaultNextGameState();
+    nextState.castleSquares[color] = empty_set;
+    AddState(nextState);
 }
 
 void MakeMove(BoardInfo_t* boardInfo, Move_t move, Color_t color) {
@@ -16,7 +72,7 @@ void MakeMove(BoardInfo_t* boardInfo, Move_t move, Color_t color) {
     
     switch (specialFlag) {
         case castle_flag:
-            CastlingMakeHandler(boardInfo, move, color);
+            MakeCastlingHandler(boardInfo, move, color);
         break;
         case promotion_flag:
         /* code */
