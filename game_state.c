844b0fa28745100be958111e776b3d120a3f8b40
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2023-02-04 01:41:59 -0500

    time to fix this

---
+++ game_state.c
@@ -0,0 +1,86 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "game_state.h"
+
+typedef struct {
+    GameState_t gameStates[GAMESTATES_MAX];
+    int top;
+} Stack_t;
+
+enum {
+    stack_empty = -1
+};
+
+static Stack_t stack = {
+    .top = stack_empty,
+};
+
+#define CurrentState(stack) stack.gameStates[stack.top]
+
+#define NextState(stack) stack.gameStates[stack.top + 1]
+
+GameState_t* GetUninitializedNextGameState() {
+    GameState_t* nextState = &(NextState(stack));
+    stack.top++;
+    return nextState;
+}
+
+GameState_t* GetDefaultNextGameState() {
+    GameState_t* defaultState = &(NextState(stack));
+
+    defaultState->capturedPiece = ReadCapturedPiece();
+    defaultState->halfmoveClock = ReadHalfmoveClock() + 1;
+    defaultState->castleSquares[white] = ReadCastleSquares(white);
+    defaultState->castleSquares[black] = ReadCastleSquares(black);
+    defaultState->enPassantSquares = empty_set;
+
+    stack.top++;
+    return defaultState;
+}
+
+void AddStartingGameState() {
+    GameState_t* gameStartState = GetUninitializedNextGameState();
+
+    gameStartState->capturedPiece = none_type;
+    gameStartState->halfmoveClock = 0;
+    gameStartState->castleSquares[white] = white_kingside_castle_sq | white_queenside_castle_sq;
+    gameStartState->castleSquares[black] = black_kingside_castle_sq | black_queenside_castle_sq;
+    gameStartState->enPassantSquares = empty_set;
+}
+
+void RevertState() {
+    assert(stack.top >= 0);
+    stack.top--;
+}
+
+Piece_t ReadCapturedPiece() {
+    return CurrentState(stack).capturedPiece;
+}
+
+HalfmoveCount_t ReadHalfmoveClock() {
+    return CurrentState(stack).halfmoveClock;
+}
+
+Bitboard_t ReadCastleSquares(Color_t color) {
+    return CurrentState(stack).castleSquares[color];
+}
+
+Bitboard_t ReadEnPassantSquares() {
+    return CurrentState(stack).enPassantSquares;
+}
+
+GameState_t ReadCurrentGameState() {
+    return CurrentState(stack);
+}
+
+GameState_t ReadDefaultNextGameState() {
+    GameState_t* nextState = GetDefaultNextGameState();
+    stack.top--;
+    return *nextState;
+}
+
+void ResetGameStateStack() {
+    stack.top = stack_empty;
+}
---
+++ game_state.h
@@ -0,0 +1,35 @@
+#ifndef __GAME_STATE_H__
+#define __GAME_STATE_H__
+
+#include <stdint.h>
+#include "board_constants.h"
+
+typedef uint16_t HalfmoveCount_t;
+typedef struct {
+    Piece_t capturedPiece;
+    HalfmoveCount_t halfmoveClock;
+    Bitboard_t enPassantSquares;
+    Bitboard_t castleSquares[2];
+} GameState_t;
+
+GameState_t* GetUninitializedNextGameState();
+
+GameState_t* GetDefaultNextGameState();
+
+void AddStartingGameState();
+
+void RevertState();
+
+Piece_t ReadCapturedPiece();
+
+HalfmoveCount_t ReadHalfmoveClock();
+
+Bitboard_t ReadCastleSquares(Color_t color);
+
+Bitboard_t ReadEnPassantSquares();
+
+GameState_t ReadDefaultNextGameState();
+
+void ResetGameStateStack();
+
+#endif
