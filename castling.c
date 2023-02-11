499fdb66b5dfa9f0558ae8e550fc69d5039a25a3
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2023-02-10 22:08:52 -0500

    reverting because made ubuntu slower

--- castling.c
+++ castling.c
@@ -1,13 +1,13 @@
 #include "castling.h"
 
 enum {
-    white_ks_bit = 0b0001,
-    black_ks_bit = 0b0010,
-    white_qs_bit = 0b0100,
-    black_qs_bit = 0b1000,
+    black_ks_bit = 0b0001,
+    black_qs_bit = 0b0010,
+    white_ks_bit = 0b0100,
+    white_qs_bit = 0b1000,
 
-    white_all_bits = 0b0101,
-    black_all_bits = 0b1010
+    black_all_bits = 0b0011,
+    white_all_bits = 0b1100
 };
 
 void InitCastleRightsToZero(CastleRights_t* castleRights) {
@@ -15,29 +15,76 @@ void InitCastleRightsToZero(CastleRights_t* castleRights) {
 }
 
 void SetKingsideCastleRights(CastleRights_t* castleRights, Color_t color) {
-    castleRights->data |= (white_ks_bit << color);
+    switch (color) {
+    case white:
+        castleRights->data |= white_ks_bit;
+        break;
+    case black:
+        castleRights->data |= black_ks_bit;
+        break;
+    }
 }
 
 void SetQueensideCastleRights(CastleRights_t* castleRights, Color_t color) {
-    castleRights->data |= (white_qs_bit << color);
+    switch (color) {
+    case white:
+        castleRights->data |= white_qs_bit;
+        break;
+    case black:
+        castleRights->data |= black_qs_bit;
+        break;
+    }
 }
 
 void ResetKingsideCastleRights(CastleRights_t* castleRights, Color_t color) {
-    castleRights->data &= ~(white_ks_bit << color);
+    switch (color) {
+    case white:
+        castleRights->data &= ~white_ks_bit;
+        break;
+    case black:
+        castleRights->data &= ~black_ks_bit;
+        break;
+    }
 }
 
 void ResetQueensideCastleRights(CastleRights_t* castleRights, Color_t color) {
-    castleRights->data &= ~(white_qs_bit << color);
+    switch (color) {
+    case white:
+        castleRights->data &= ~white_qs_bit;
+        break;
+    case black:
+        castleRights->data &= ~black_qs_bit;
+        break;
+    }
 }
 
 void ResetAllRights(CastleRights_t* castleRights, Color_t color) {
-    castleRights->data &= ~(white_all_bits << color);
+    switch (color) {
+    case white:
+        castleRights->data &= ~white_all_bits;
+        break;
+    case black:
+        castleRights->data &= ~black_all_bits;
+        break;
+    }
 }
 
 bool ReadKingsideCastleRights(CastleRights_t castleRights, Color_t color) {
-    return castleRights.data & (white_ks_bit << color);
+    switch (color) {
+    case white:
+        return castleRights.data & white_ks_bit;
+
+    case black:
+        return castleRights.data & black_ks_bit;
+    }
 }
 
 bool ReadQueensideCastleRights(CastleRights_t castleRights, Color_t color) {
-    return castleRights.data & (white_qs_bit << color);
+    switch (color) {
+    case white:
+        return castleRights.data & white_qs_bit;
+
+    case black:
+        return castleRights.data & black_qs_bit;
+    }
 }
