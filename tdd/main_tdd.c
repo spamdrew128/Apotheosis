f1429a64ded5413b4e76731b9a96907b30391a00
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2023-02-18 03:45:59 -0500

    working

--- tdd/main_tdd.c
+++ tdd/main_tdd.c
@@ -55,7 +55,7 @@ int main(int argc, char** argv)
     BoardInfo_t boardInfo;
     GameStack_t gameStack;
     ZobristStack_t zobristStack;
-    bool running = false;
+    bool running = true;
     while(running)
     {
         running = InterpretUCIInput(&boardInfo, &gameStack, &zobristStack);
