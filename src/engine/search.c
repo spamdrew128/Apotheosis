28be381698cd2f66e0e75131b95f2d59054e8215
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2023-02-17 23:58:12 -0500

    omw

--- src/engine/search.c
+++ src/engine/search.c
@@ -4,11 +4,44 @@
 #include "movegen.h"
 #include "make_and_unmake.h"
 #include "RNG.h"
+#include "endings.h"
 
 typedef uint8_t Depth_t;
 
-static SearchResults_t NegamaxRoot(
-    MoveList_t* moveList,     
+static EvalScore_t NegamaxHelper(
+    BoardInfo_t* boardInfo,
+    GameStack_t* gameStack,
+    ZobristStack_t* zobristStack,
+    Depth_t depth
+)
+{
+    if(depth == 0) {
+        return ScoreOfPosition(boardInfo);
+    }
+
+    Move_t bestMove;
+    EvalScore_t bestScore = -EVAL_MAX;
+    
+    MoveList_t moveList;
+    CompleteMovegen(&moveList, boardInfo, gameStack);
+    for(int i = 0; i <= moveList.maxIndex; i++) {
+        Move_t move = moveList.moves[i];
+        MakeMove(boardInfo, gameStack, move);
+
+        EvalScore_t score = -NegamaxHelper(boardInfo, gameStack, zobristStack, depth-1);
+
+        UnmakeMove(boardInfo, gameStack);
+
+        if(score > bestScore) {
+            bestScore = score;
+            bestMove = move;
+        }
+    }
+
+    return bestScore;
+}
+
+static SearchResults_t NegamaxRoot(  
     BoardInfo_t* boardInfo,
     GameStack_t* gameStack,
     ZobristStack_t* zobristStack,
@@ -16,13 +49,29 @@ static SearchResults_t NegamaxRoot(
 )
 {
     Move_t bestMove;
-    Centipawns_t bestScore = -EVAL_MAX;
-
-    CompleteMovegen(moveList, boardInfo, gameStack);
-    for(int i = 0; i <= moveList->maxIndex; i++) {
-        Move_t move = moveList->moves[i];
+    EvalScore_t bestScore = -EVAL_MAX;
+    
+    MoveList_t moveList;
+    CompleteMovegen(&moveList, boardInfo, gameStack);
+    for(int i = 0; i <= moveList.maxIndex; i++) {
+        Move_t move = moveList.moves[i];
         MakeMove(boardInfo, gameStack, move);
+
+        EvalScore_t score = -NegamaxHelper(boardInfo, gameStack, zobristStack, depth-1);
+
+        UnmakeMove(boardInfo, gameStack);
+
+        if(score > bestScore) {
+            bestScore = score;
+            bestMove = move;
+        }
     }
+
+    SearchResults_t results;
+    results.score = bestScore;
+    results.bestMove = bestMove;
+
+    return results;
 }
 
 SearchResults_t Search(
@@ -32,10 +81,7 @@ SearchResults_t Search(
     ZobristStack_t* zobristStack
 )
 {
-    MoveList_t moveList;
-    CompleteMovegen(&moveList, boardInfo, gameStack);
-
-    SearchResults_t results = NegamaxRoot(&moveList, boardInfo, gameStack, zobristStack, 3);
+    SearchResults_t results = NegamaxRoot(boardInfo, gameStack, zobristStack, 3);
 
     return results;
 }
