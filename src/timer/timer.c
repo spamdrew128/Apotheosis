aa9e5b3f3cfacd2b3d00888535b7b627fb438412
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2023-02-18 19:14:32 -0500

    added timers

--- src/timer/timer.c
+++ src/timer/timer.c
@@ -2,4 +2,11 @@
 
 #include "timer.h"
 
+void TimerInit(Timer_t* timer, Milliseconds_t duration) {
+    timer->startTime = ((Seconds_t)clock()) / CLOCKS_PER_SEC;
+    timer->endTime = (((Seconds_t)duration) / msec_per_sec) + timer->startTime;
+}
 
+bool TimerExpired(Timer_t* timer) {
+    return (Seconds_t)clock() > timer->endTime;
+}
--- src/timer/timer.h
+++ src/timer/timer.h
@@ -1,13 +1,18 @@
 #ifndef __TIMER_H__
 #define __TIMER_H__
 
+#include <stdbool.h>
 #include <stdint.h>
 
 #include "time_constants.h"
 
 typedef struct {
     Seconds_t startTime;
-    Seconds_t duration;
+    Seconds_t endTime;
 } Timer_t;
+
+void TimerInit(Timer_t* timer, Milliseconds_t duration);
+
+bool TimerExpired(Timer_t* timer);
 
 #endif
