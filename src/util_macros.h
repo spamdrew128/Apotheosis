5eb7cbe5f64a9e05fb918df1e4a5e19820b6dcda
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2023-02-19 20:08:40 -0500

    finished writing test

---
+++ src/util_macros.h
@@ -0,0 +1,6 @@
+#ifndef __UTIL_MACROS_H__
+#define __UTIL_MACROS_H__
+
+#define NUM_ARRAY_ELEMENTS(array) sizeof(array) / sizeof(*array)
+
+#endif
--- tdd/engine_tests/PV_table_tdd.c
+++ tdd/engine_tests/PV_table_tdd.c
@@ -1,8 +1,91 @@
 #include "PV_table_tdd.h"
+#include "debug.h"
 
-static PvTable_t pvTable;
+// assumes white plays first
+static EvalScore_t NegamaxFakeout(
+    PvTable_t* pvTable,
+    Depth_t depth,
+    Ply_t ply,
+    int branchingFactor,
+    int* leafCount,
+    const int* evaluations
+)
+{
+    if(depth == 0) {
+        int eval = evaluations[*leafCount];
+        (*leafCount)++;
+        return eval;
+    }
+
+    EvalScore_t bestScore = -EVAL_MAX;
+    for(uint16_t i = 0; i < branchingFactor; i++) {
+        Move_t move;
+        move.data = i;
+    
+        EvalScore_t score =
+            -NegamaxFakeout(
+                pvTable,
+                depth-1,
+                ply+1,
+                branchingFactor,
+                leafCount,
+                evaluations
+            );
+
+        if(score > bestScore) {
+            UpdatePvTable(pvTable, move, ply);
+        }
+    }
+}
+
+static bool PvMatchesExpectedPath(PvTable_t* pvTable, uint16_t* expectedPathFromRoot) {
+    bool success = true;
+    for(int i = 0; i < pvTable->maxPly; i++) {
+        if(pvTable->moveMatrix[0][i].data != expectedPathFromRoot[i]) {
+            return false;
+        }
+    }
+}
+
+static bool FindsCorrectPv(
+    Depth_t depth,
+    int branchingFactor,
+    const int* evaluations,
+    const uint16_t* expectedPathFromRoot
+)
+{
+    PvTable_t pvTable;
+    PvTableInit(&pvTable, depth);
+    int leafCount = 0;
+
+    NegamaxFakeout(
+        &pvTable,
+        depth,
+        0,
+        branchingFactor,
+        &leafCount,
+        evaluations
+    );
+
+    bool success = PvMatchesExpectedPath(&pvTable, expectedPathFromRoot);
+
+    PvTableTeardown(&pvTable, depth);
+}
+
+static void ShouldFindCorrectPv() {
+    int evaluations[] = { -2, 2, 1, 3, -3, 1, -4, -2 };
+    uint16_t expectedPathFromRoot[] = { 0, 0, 1 };
+    bool success = 
+        FindsCorrectPv(
+            3,
+            2,
+            evaluations,
+            expectedPathFromRoot
+        );
+
+    PrintResults(success);
+}
 
 void PvTableTDDRunner() {
-    PvTableInit(&pvTable, 3);
-    PvTableTeardown(&pvTable, 3);
+    ShouldFindCorrectPv();
 }
