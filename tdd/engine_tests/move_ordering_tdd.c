a92e000352c7f6d2e718a9d45c9e6ac9351a26d4
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2023-03-04 14:45:03 -0500

    tests pass

--- tdd/engine_tests/move_ordering_tdd.c
+++ tdd/engine_tests/move_ordering_tdd.c
@@ -16,19 +16,31 @@ static EvalScore_t MVVScore(Move_t capture) {
 
     Piece_t victim = PieceOnSquare(&boardInfo, toSquare);
     Piece_t attacker = PieceOnSquare(&boardInfo, fromSquare);
-    
-    assert(victim != none_type);
 
     return ValueOfPiece(victim) - ValueOfPiece(attacker);
 }
 
-static bool CapturesAreCorrectlyOrdered() {
+static bool MoveIsCapture(Move_t capture) {
+    Piece_t victim = PieceOnSquare(&boardInfo, ReadToSquare(capture));
+    
+    return (victim != none_type || ReadSpecialFlag(capture) == en_passant_flag);
+}
+
+static bool CapturesAreCorrectlyOrdered(MoveListIterator_t* iterator) {
+    Move_t prevMove = IteratorGetNextMove(iterator);
+
     for(int i = 1; i <= moveList.maxCapturesIndex; i++) {
-        Move_t prevMove = moveList.moves[i-1];
-        Move_t currentMove = moveList.moves[i];
+        Move_t currentMove = IteratorGetNextMove(iterator);
+
+        if(!MoveIsCapture(prevMove) || !MoveIsCapture(currentMove)) {
+            return false;
+        }
+
         if(MVVScore(prevMove) < MVVScore(currentMove)) {
             return false;
         }
+
+        prevMove = currentMove;
     }
 
     return true;
@@ -38,9 +50,11 @@ static void ShouldOrderCaptures() {
     FEN_t manyCapturesFen = "rnb1kb1r/p4ppp/2p5/4N3/1ppqP1n1/2P1BQ1P/PP3PP1/RN2K2R w KQkq - 2 10";
     InterpretFEN(manyCapturesFen, &boardInfo, &gameStack, &zobristStack);
     CompleteMovegen(&moveList, &boardInfo, &gameStack);
-    SortMoveList(&moveList, &boardInfo);
+    
+    MoveListIterator_t iterator;
+    MoveListIteratorInit(&iterator, &moveList, &boardInfo, moveList.maxIndex);
 
-    PrintResults(CapturesAreCorrectlyOrdered());
+    PrintResults(CapturesAreCorrectlyOrdered(&iterator));
 }
 
 void MoveOrderingTDDRunner() {
