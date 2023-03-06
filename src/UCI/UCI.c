9b44891a54fc3100bd7bfedc584f14f9384d5c30
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2023-03-06 08:40:34 -0500

    fixed version issue

--- src/UCI/UCI.c
+++ src/UCI/UCI.c
@@ -14,7 +14,7 @@
 
 #define BUFFER_SIZE 50000
 
-#define ENGINE_ID "id name Apotheosis\nid author Spamdrew\n"
+#define ENGINE_ID "id name Apotheosis v1.0.1\nid author Spamdrew\n"
 #define UCI_OK "uciok\n"
 #define READY_OK "readyok\n"
 
