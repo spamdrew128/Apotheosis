216913f1fdd16ace54159a82a43c2f5d026e03cf
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2023-03-12 12:27:45 -0400

    modified logic

--- src/engine/history.c
+++ src/engine/history.c
@@ -1,7 +1,6 @@
 #include <assert.h>
 
 #include "history.h"
-#include "util_macros.h"
 
 enum {
     history_cap = 1200
@@ -19,8 +18,10 @@ void UpdateHistory(History_t* history, BoardInfo_t* boardInfo, Move_t move, Dept
     Piece_t piece = PieceOnSquare(boardInfo, ReadFromSquare(move));
     Square_t toSquare = ReadToSquare(move);
 
-    MoveScore_t currentScore = history->scores[piece][toSquare];
-    history->scores[piece][toSquare] += MIN(depth * depth, history_cap);
+    history->scores[piece][toSquare] += depth * depth;
+    if(history->scores[piece][toSquare] > history_cap) {
+       history->scores[piece][toSquare] = history_cap;
+    }
 }
 
 MoveScore_t HistoryScore(History_t* history, BoardInfo_t* boardInfo, Move_t move) {
