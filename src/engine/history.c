f7c28afa066e051715ba773f9d036b3cf47c44fc
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2023-03-11 23:00:34 -0500

    unsure if this is right

--- src/engine/history.c
+++ src/engine/history.c
@@ -2,6 +2,10 @@
 
 #include "history.h"
 
+enum {
+    decay_threshold = 10000
+};
+
 void InitHistory(History_t* history) {
     for(int i = 0; i < NUM_PIECES; i++) {
         for(int j = 0; j < NUM_SQUARES; j++) {
@@ -10,10 +14,22 @@ void InitHistory(History_t* history) {
     }
 }
 
+static void DecayHistoryTable(History_t* history) {
+    for(int i = 0; i < NUM_PIECES; i++) {
+        for(int j = 0; j < NUM_SQUARES; j++) {
+            history->scores[i][j] /= 2;
+        }
+    }
+}
+
 void UpdateHistory(History_t* history, BoardInfo_t* boardInfo, Move_t move, Depth_t depth) {
     Piece_t piece = PieceOnSquare(boardInfo, ReadFromSquare(move));
     Square_t toSquare = ReadToSquare(move);
     history->scores[piece][toSquare] += depth * depth;
+
+    if(history->scores[piece][toSquare] > decay_threshold) {
+        DecayHistoryTable(history);
+    }
 }
 
 MoveScore_t HistoryScore(History_t* history, BoardInfo_t* boardInfo, Move_t move) {
