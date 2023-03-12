206e4bde09322aae0f1c29bb2eee924ebaa31634
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2023-03-12 01:30:18 -0500

    trying with color

--- src/engine/history.c
+++ src/engine/history.c
@@ -3,21 +3,22 @@
 #include "history.h"
 
 void InitHistory(History_t* history) {
-    for(int i = 0; i < NUM_SQUARES; i++) {
+    for(int i = 0; i < NUM_PIECES; i++) {
         for(int j = 0; j < NUM_SQUARES; j++) {
-            history->scores[i][j] = 0;
+            history->scores[white][i][j] = 0;
+            history->scores[black][i][j] = 0;
         }
     }
 }
 
 void UpdateHistory(History_t* history, BoardInfo_t* boardInfo, Move_t move, Depth_t depth) {
-    Square_t fromSquare = ReadFromSquare(move);
+    Piece_t piece = PieceOnSquare(boardInfo, ReadFromSquare(move));
     Square_t toSquare = ReadToSquare(move);
-    history->scores[fromSquare][toSquare] += depth * depth;
+    history->scores[boardInfo->colorToMove][piece][toSquare] += depth * depth;
 }
 
 MoveScore_t HistoryScore(History_t* history, BoardInfo_t* boardInfo, Move_t move) {
-    Square_t fromSquare = ReadFromSquare(move);
+    Piece_t piece = PieceOnSquare(boardInfo, ReadFromSquare(move));
     Square_t toSquare = ReadToSquare(move);
-    return history->scores[fromSquare][toSquare];
+    return history->scores[boardInfo->colorToMove][piece][toSquare];
 }
