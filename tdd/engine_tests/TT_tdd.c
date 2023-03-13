13d55385c362c2cb8c7d5861655d7dbf351f913a
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2023-03-12 22:21:19 -0400

    fixed tests

--- tdd/engine_tests/TT_tdd.c
+++ tdd/engine_tests/TT_tdd.c
@@ -111,4 +111,6 @@ void TranspositionTableTDDRunner() {
     ShouldCutoffIfScoreIsExact();
     ShouldCutoffIfScoreIsLowerBoundAndGreaterThanBeta();
     ShouldCutoffIfScoreIsUpperBoundAndLessThanAlpha();
+
+    ShouldAdjustMateScores();
 }
