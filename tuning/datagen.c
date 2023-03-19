609aab677b47aafe7ba8efbc5a16c6a958f391eb
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2023-03-19 03:38:25 -0400

    added random moves

--- tuning/datagen.c
+++ tuning/datagen.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
+#include "util_macros.h"
 #include "datagen.h"
 #include "uci.h"
 #include "RNG.h"
 #include "endings.h"
 #include "FEN.h"
+#include "movegen.h"
+#include "make_and_unmake.h"
 
 enum {
     NUM_GAMES = 10000,
@@ -13,13 +17,20 @@ enum {
     RAND_PLY_COUNT = 8,
 };
 
-static void GameLoop(UciSearchInfo_t* searchInfo) {
+static void GameLoop(UciApplicationData_t* data) {
 
 }
 
-static void RandomMoves(UciSearchInfo_t* searchInfo, Generator_t* generator) {
+static void RandomMoves(UciApplicationData_t* data, Generator_t* generator) {
     for(int i = 0; i < RAND_PLY_COUNT; i++) {
+        MoveEntryList_t moveList;
+        CompleteMovegen(&moveList, &data->boardInfo, &data->gameStack);
 
+        assert(moveList.maxIndex != movelist_empty);
+        int index = RandUnsigned64(generator) % (moveList.maxIndex + 1);
+
+        Move_t move = moveList.moves[index].move;
+        MakeMove(&data->boardInfo, &data->gameStack, move);
     }
 }
 
@@ -38,9 +49,9 @@ void GenerateData(const char* filename) {
         searchInfo->overhead = 0;
         searchInfo->forceTime = TIME_PER_MOVE;
 
-        RandomMoves(&searchInfo, &generator);
+        RandomMoves(&data, &generator);
 
-        GameLoop(&searchInfo);
+        GameLoop(&data);
 
         TeardownTT(&searchInfo->tt);
     }
