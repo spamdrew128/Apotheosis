e9de060d8fc97511fd688f6985629d4b016be3fb
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2023-03-19 03:29:48 -0400

    omw

--- tuning/datagen.c
+++ tuning/datagen.c
@@ -5,41 +5,44 @@
 #include "uci.h"
 #include "RNG.h"
 #include "endings.h"
+#include "FEN.h"
 
 enum {
     NUM_GAMES = 10000,
     TIME_PER_MOVE = 100,
+    RAND_PLY_COUNT = 8,
 };
 
-static void GameLoop(UciSearchInfo_t* searchInfo, Generator_t* generator) {
+static void GameLoop(UciSearchInfo_t* searchInfo) {
 
 }
 
-static void RandomMoves(UciSearchInfo_t* searchInfo) {
+static void RandomMoves(UciSearchInfo_t* searchInfo, Generator_t* generator) {
+    for(int i = 0; i < RAND_PLY_COUNT; i++) {
 
+    }
 }
 
 void GenerateData(const char* filename) {
     FILE* fp = fopen(filename, "wb");
 
-    UciSearchInfo_t searchInfo;
-    BoardInfo_t boardInfo;
-    GameStack_t gameStack;
-    ZobristStack_t zobristStack;
+    UciApplicationData_t data;
 
     Generator_t generator;
     InitRNG(&generator, false);
 
     for(int i = 0; i < NUM_GAMES; i++) {
-        UciSearchInfoInit(&searchInfo);
-        searchInfo.overhead = 0;
-        searchInfo.forceTime = TIME_PER_MOVE;
+        UciApplicationDataInit(&data);
 
-        RandomMoves(&searchInfo);
+        UciSearchInfo_t* searchInfo = &data.uciSearchInfo;
+        searchInfo->overhead = 0;
+        searchInfo->forceTime = TIME_PER_MOVE;
 
-        GameLoop(&searchInfo, &generator);
+        RandomMoves(&searchInfo, &generator);
 
-        TeardownTT(&searchInfo.tt);
+        GameLoop(&searchInfo);
+
+        TeardownTT(&searchInfo->tt);
     }
 
     fclose(fp);
