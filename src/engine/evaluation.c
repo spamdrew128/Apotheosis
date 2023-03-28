4eec138357ea159a5466bf4932ed394c5e573596
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2023-03-27 20:07:04 -0400

    added eval

--- src/engine/evaluation.c
+++ src/engine/evaluation.c
@@ -5,13 +5,11 @@
 #include "util_macros.h"
 #include "legals.h"
 
-enum {
-    mobility_weight = 5
-};
-
 static Centipawns_t midgamePST[6][NUM_SQUARES] = { {KNIGHT_MG_PST}, {BISHOP_MG_PST}, {ROOK_MG_PST}, {QUEEN_MG_PST}, {PAWN_MG_PST}, {KING_MG_PST} };
 static Centipawns_t endgamePST[6][NUM_SQUARES] = { {KNIGHT_EG_PST}, {BISHOP_EG_PST}, {ROOK_EG_PST}, {QUEEN_EG_PST}, {PAWN_EG_PST}, {KING_EG_PST} };
 static Phase_t gamePhaseLookup[6] = { KNIGHT_PHASE_VALUE, BISHOP_PHASE_VALUE, ROOK_PHASE_VALUE, QUEEN_PHASE_VALUE, PAWN_PHASE_VALUE, KING_PHASE_VALUE };
+
+static Centipawns_t bishopPairBonus[NUM_PHASES] = { BISHOP_PAIR_BONUS };
 
 static void PSTEval(
     Bitboard_t infoField[2],
@@ -40,35 +38,35 @@ static void PSTEval(
     }
 }
 
-static Centipawns_t MaterialBalanceAndPSTBonus(BoardInfo_t* boardInfo) {
-    Centipawns_t mgScore = 0;
-    Centipawns_t egScore = 0;
-    Phase_t gamePhase = 0;
-
-    PSTEval(boardInfo->knights, knight, &gamePhase, &mgScore, &egScore);
-    PSTEval(boardInfo->bishops, bishop, &gamePhase, &mgScore, &egScore);
-    PSTEval(boardInfo->rooks, rook, &gamePhase, &mgScore, &egScore);
-    PSTEval(boardInfo->queens, queen, &gamePhase, &mgScore, &egScore);
-    PSTEval(boardInfo->pawns, pawn, &gamePhase, &mgScore, &egScore);
-    PSTEval(boardInfo->kings, king, &gamePhase, &mgScore, &egScore);
-
-    Phase_t mgPhase = MIN(gamePhase, PHASE_MAX);
-    Phase_t egPhase = PHASE_MAX - mgPhase;
-    return (mgScore * mgPhase + egScore * egPhase) / PHASE_MAX; // weighted average
+static void MaterialBalanceAndPSTBonus(BoardInfo_t* boardInfo, Phase_t* phase, Centipawns_t* mgScore, Centipawns_t* egScore) {
+    PSTEval(boardInfo->knights, knight, phase, mgScore, egScore);
+    PSTEval(boardInfo->bishops, bishop, phase, mgScore, egScore);
+    PSTEval(boardInfo->rooks, rook, phase, mgScore, egScore);
+    PSTEval(boardInfo->queens, queen, phase, mgScore, egScore);
+    PSTEval(boardInfo->pawns, pawn, phase, mgScore, egScore);
+    PSTEval(boardInfo->kings, king, phase, mgScore, egScore);
 }
 
-static Centipawns_t MobilityEval(BoardInfo_t* boardInfo) {
-    Bitboard_t empty = boardInfo->empty;
-    Bitboard_t whitePseudolegals = AttackedSquares(boardInfo, empty, white) & ~boardInfo->allPieces[white];
-    Bitboard_t blackPseudolegals = AttackedSquares(boardInfo, empty, black) & ~boardInfo->allPieces[black];
-
-    return (PopCount(whitePseudolegals) - PopCount(blackPseudolegals)) * mobility_weight;
+static void BishopPairBonus(BoardInfo_t* boardInfo, Centipawns_t* mgScore, Centipawns_t* egScore) {
+    if(PopCount(boardInfo->bishops[white] >= 2)) {
+        *mgScore += bishopPairBonus[mg_phase];
+        *egScore += bishopPairBonus[eg_phase];
+    }
+    if(PopCount(boardInfo->bishops[black] >= 2)) {
+        *mgScore -= bishopPairBonus[mg_phase];
+        *egScore -= bishopPairBonus[eg_phase];
+    }
 }
 
 EvalScore_t ScoreOfPosition(BoardInfo_t* boardInfo) {
-    EvalScore_t eval = 0;
-    eval += MaterialBalanceAndPSTBonus(boardInfo);
-    // eval += MobilityEval(boardInfo);
+    Phase_t phase = 0;
+    Centipawns_t mgScore = 0;
+    Centipawns_t egScore = 0;
+
+    MaterialBalanceAndPSTBonus(boardInfo, &phase, &mgScore, &egScore);
+    BishopPairBonus(boardInfo, &mgScore, &egScore);
+
+    EvalScore_t eval = (mgScore*phase + egScore*(PHASE_MAX - phase)) / PHASE_MAX;
 
     return boardInfo->colorToMove == white ? eval : -eval;
 }
