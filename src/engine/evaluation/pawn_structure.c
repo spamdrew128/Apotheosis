cb8876936301999635ccfcdae685532a5242e6f8
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2023-04-03 20:52:19 -0400

    why you fail

--- src/engine/evaluation/pawn_structure.c
+++ src/engine/evaluation/pawn_structure.c
@@ -6,23 +6,31 @@
 
 static Centipawns_t passerBonus[2] = { PASSED_PAWN_BONUS }; 
 
-static Bitboard_t WhiteFrontSpan(Bitboard_t b) {
-    b |= GenShiftNorth(b, 1);
-    b |= GenShiftNorth(b, 2);
-    b |= GenShiftNorth(b, 4);
-    return b;
-}
-
-static Bitboard_t BlackFrontSpan(Bitboard_t b) {
+static Bitboard_t WhiteFill(Bitboard_t b) {
     b |= GenShiftSouth(b, 1);
     b |= GenShiftSouth(b, 2);
     b |= GenShiftSouth(b, 4);
     return b;
 }
 
+static Bitboard_t BlackFill(Bitboard_t b) {
+    b |= GenShiftSouth(b, 1);
+    b |= GenShiftSouth(b, 2);
+    b |= GenShiftSouth(b, 4);
+    return b;
+}
+
+static Bitboard_t WhiteForwardFill(Bitboard_t b) {
+    return NortOne(WhiteFill(b));
+}
+
+static Bitboard_t BlackForwardFill(Bitboard_t b) {
+    return SoutOne(BlackFill(b));
+}
+
 void PassedPawns(BoardInfo_t* boardInfo, Centipawns_t* mgScore, Centipawns_t* egScore) {
-    const Bitboard_t wFrontSpan = WhiteFrontSpan(boardInfo->pawns[white]);
-    const Bitboard_t bFrontSpan = BlackFrontSpan(boardInfo->pawns[black]);
+    const Bitboard_t wFrontSpan = WhiteForwardFill(boardInfo->pawns[white]);
+    const Bitboard_t bFrontSpan = BlackForwardFill(boardInfo->pawns[black]);
 
     const Bitboard_t wPawnBlocks = wFrontSpan | EastOne(wFrontSpan) | WestOne(wFrontSpan);
     const Bitboard_t bPawnBlocks = bFrontSpan | EastOne(bFrontSpan) | WestOne(bFrontSpan);
