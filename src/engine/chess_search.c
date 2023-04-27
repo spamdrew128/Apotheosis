7ba12b60411277cf31bd7872ec58a1ed2b424e82
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2023-04-27 05:47:36 -0400

    nuked comment

--- src/engine/chess_search.c
+++ src/engine/chess_search.c
@@ -246,7 +246,7 @@ static EvalScore_t Negamax(
         const EvalScore_t staticEval = ScoreOfPosition(boardInfo);
 
         // NULL MOVE PRUNING
-        if(depth >= NMP_MIN_DEPTH && doNullMove && !WeOnlyHavePawnsOnBoard(boardInfo)) { // && !OnlyPawnsOnBoard(boardInfo)
+        if(depth >= NMP_MIN_DEPTH && doNullMove && !WeOnlyHavePawnsOnBoard(boardInfo)) {
             const int reduction = 3 + depth / 5;
             const int depthPrime = depth - reduction;
             assert(depthPrime >= 0);
