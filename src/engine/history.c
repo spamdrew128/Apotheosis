bbf9c8b5f93c5453fec4456b1e9dfa8a0f42457c
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2023-04-27 05:50:36 -0400

    color added

--- src/engine/history.c
+++ src/engine/history.c
@@ -10,24 +10,27 @@ enum {
 void InitHistory(History_t* history) {
     for(int i = 0; i < NUM_PIECES; i++) {
         for(int j = 0; j < NUM_SQUARES; j++) {
-            history->scores[i][j] = 0;
+            history->scores[white][i][j] = 0;
+            history->scores[black][i][j] = 0;
         }
     }
 }
 
 void UpdateHistory(History_t* history, BoardInfo_t* boardInfo, Move_t move, Depth_t depth) {
-    Piece_t piece = PieceOnSquare(boardInfo, ReadFromSquare(move));
-    Square_t toSquare = ReadToSquare(move);
+    const Piece_t piece = PieceOnSquare(boardInfo, ReadFromSquare(move));
+    const Square_t toSquare = ReadToSquare(move);
+    const Color_t color = boardInfo->colorToMove;
+    history->scores[color][piece][toSquare] += MIN(depth * depth, bonus_max);
 
-    history->scores[piece][toSquare] += MIN(depth * depth, bonus_max);
-
-    if(history->scores[piece][toSquare] > history_max) {
-        history->scores[piece][toSquare] = history_max;
+    if(history->scores[color][piece][toSquare] > history_max) {
+        history->scores[color][piece][toSquare] = history_max;
     }
 }
 
 MoveScore_t HistoryScore(History_t* history, BoardInfo_t* boardInfo, Move_t move) {
-    Piece_t piece = PieceOnSquare(boardInfo, ReadFromSquare(move));
-    Square_t toSquare = ReadToSquare(move);
-    return history->scores[piece][toSquare];
+    const Piece_t piece = PieceOnSquare(boardInfo, ReadFromSquare(move));
+    const Square_t toSquare = ReadToSquare(move);
+    const Color_t color = boardInfo->colorToMove;
+
+    return history->scores[color][piece][toSquare];
 }
