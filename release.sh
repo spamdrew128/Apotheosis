27a5d1934efb0ee5b7c70e9cecca2bebd0d78b06
spamdrew128 <72526028+spamdrew128@users.noreply.github.com>
2023-04-28 23:45:03 -0400

    update naming

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 # for some reason I cant put the full version number here or else it breaks, 
 # just rename the EXE yourself I apologize.
-make Apotheosis_Dev EXE=Apotheosis_Dev RELEASE=Y
+make ApotheosisV4 EXE=ApotheosisV4 RELEASE=Y
